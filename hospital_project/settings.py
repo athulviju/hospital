416517713f4b2a3184354c19fc301e915001e40d
athulviju <athulviju2007@gmail.com>
2026-07-27 12:44:18 +0530

    add modification file

--- hospital_project/settings.py
+++ hospital_project/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4h#x&@n=s@7x5)_!t#yjx_7$)8-##k6fg#ieri!oy0ms0-o1e8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
